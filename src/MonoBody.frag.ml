4665a9531686d74f01d6e62e20e107bdc8feb67a
François Pottier <francois.pottier@inria.fr>
2024-06-26 22:41:25 +0200

    In MonoBody, exchange two segments of the file.

--- src/MonoBody.frag.ml
+++ src/MonoBody.frag.ml
@@ -2,6 +2,35 @@
 
 (* -------------------------------------------------------------------------- *)
 
+(* Types. *)
+
+type element = X.t
+
+type length = int
+type capacity = int
+type index = int
+
+(* In [make] and in [set_higher_capacity], the allocation of an array of
+   size [capacity] is delayed, because we do not have a value of type ['a]
+   at hand. *)
+
+type vector = {
+
+  (* The logical length of the vector. *)
+  mutable length   : int;
+
+  (* The desired physical capacity of the vector. We impose the invariant
+     [length = 0 || A.length data = capacity]. That is, unless the vector
+     is logically empty, [capacity] is the length of the [data] array. *)
+  mutable capacity : int;
+
+  (* The data array. *)
+  mutable data     : element array;
+
+}
+
+(* -------------------------------------------------------------------------- *)
+
 module A = struct
 
   open Array
@@ -70,35 +99,6 @@ module A = struct
     done
 
 end
-
-(* -------------------------------------------------------------------------- *)
-
-(* Types. *)
-
-type element = X.t
-
-type length = int
-type capacity = int
-type index = int
-
-(* In [make] and in [set_higher_capacity], the allocation of an array of
-   size [capacity] is delayed, because we do not have a value of type ['a]
-   at hand. *)
-
-type vector = {
-
-  (* The logical length of the vector. *)
-  mutable length   : int;
-
-  (* The desired physical capacity of the vector. We impose the invariant
-     [length = 0 || A.length data = capacity]. That is, unless the vector
-     is logically empty, [capacity] is the length of the [data] array. *)
-  mutable capacity : int;
-
-  (* The data array. *)
-  mutable data     : element array;
-
-}
 
 (* -------------------------------------------------------------------------- *)
 
