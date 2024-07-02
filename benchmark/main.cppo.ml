49f922f425087e785b78c30bcdcb40e30e4865b5
François Pottier <francois.pottier@inria.fr>
2024-07-02 05:55:52 +0200

    Remove the [scan] benchmark.
    I have not found it to be reliable or useful.

--- benchmark/main.cppo.ml
+++ benchmark/main.cppo.ml
@@ -134,71 +134,20 @@ let sets n =
 
 (* -------------------------------------------------------------------------- *)
 
-(* This benchmark allocates a lot of memory in the presence of a large vector.
-   We test whether using an unscanned vector (Hector.Int) or a scanned vector
-   (Dynarray, Hector.Poly) makes a difference. Apparently, it does not. *)
-
-let allocate_a_lot n =
-  Array.init n @@ fun k ->
-  List.init n @@ fun j ->
-  k * j
-
-#define SCAN(candidate, create, push, length, n) \
-( \
-  let basis = 1 \
-  and name = sprintf "scan (size %d) (%s)" n candidate \
-  and run () = \
-    (* Create an inert vector of size [n*n]: *) \
-    let v = create () in \
-    let size = n*n in \
-    for i = 0 to size-1 do \
-      let dummy = i in \
-      push v dummy \
-    done; \
-    fun () -> \
-      (* Allocate [3k*n*n] words: *) \
-      let k = 10 in \
-      for _ = 1 to k do \
-        allocate_a_lot n \
-        |> Sys.opaque_identity |> ignore \
-      done; \
-      (* Do something to ensure that [v] is live: *) \
-      ignore (length v) \
-  in \
-  B.benchmark ~name ~quota ~basis ~run \
-)
-
-module N = struct
-  let create () = ()
-  let add_last () _x = ()
-  let length () = 0
-end
-
-let scans n =
-  [
-    SCAN("none", N.create, N.add_last, N.length, n);
-    SCAN("dynarray", R.create, R.add_last, R.length, n);
-    SCAN("poly", P.create, P.add_last, P.length, n);
-    SCAN("int", I.create, I.add_last, I.length, n);
-  ]
-
-(* -------------------------------------------------------------------------- *)
-
 (* Read the command line. *)
 
-let push, get, set, scan =
-  ref 0, ref 0, ref 0, ref 0
+let push, get, set =
+  ref 0, ref 0, ref 0
 
 let () =
   Arg.parse [
     "--push", Arg.Set_int push, " <n> Benchmark push";
     "--get", Arg.Set_int get, " <n> Benchmark get";
     "--set", Arg.Set_int set, " <n> Benchmark set";
-    "--scan", Arg.Set_int scan, " <n> Measure the cost of GC scans";
   ] (fun _ -> ()) "Invalid usage"
 
-let push, get, set, scan =
-  !push, !get, !set, !scan
+let push, get, set =
+  !push, !get, !set
 
 let possibly n (benchmarks : int -> B.benchmark list) =
   if n > 0 then run (benchmarks n)
@@ -211,5 +160,4 @@ let () =
   possibly push pushes;
   possibly get gets;
   possibly set sets;
-  possibly scan scans;
   ()
