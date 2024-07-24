770e0c68386ec1ea334c93ca32f8e4c4cdb5fec5
François Pottier <francois.pottier@inria.fr>
2024-07-24 09:05:50 +0200

    Remove [alloc] from the signature [MONOARRAY].

--- src/IntArray.mli
+++ src/IntArray.mli
@@ -1,3 +1,14 @@
 open ArraySignature
 
 include MONOARRAY with type element = int
+
+(* This module provides [alloc], but we do not include this function
+   in the signature [MONOARRAY], because this signature describes the
+   minimal requirements of our vector implementation: it serves as an
+   input signature for the functor [Mono.OutOfArray]. *)
+
+(**[alloc n d] returns a new array of length [n]. The dummy element [d]
+   may be used to initialize this array, but this is not guaranteed.
+   Thus, {b this array must be considered uninitialized}: every slot
+   must be written before it is read. *)
+val alloc : length -> dummy -> t
